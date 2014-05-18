56301eed1dceeacb1abdf20b880638e5f6ecbfff
Mate Kovacs <mate.kovacs@prezi.com>
2014-05-18 16:00:14 +0200

    work on Glyph description parsing and rendering

--- src/main/Glyph.hs
+++ src/main/Glyph.hs
@@ -1,21 +1,67 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings
+           , LambdaCase
+           , RecordWildCards
+           #-}
 module Glyph
-    ( parseKVG
-    , writeKVG
+    ( parseKvg
+    , writeKvg
+    , renderSvg
     ) where
 
 import Text.XML.Light as X
 import Text.XML.Light.Input as XI
+import Text.XML.Light.Output as XO
 
-data KVG
+data Glyph
     = Path { pathData :: String }
     | Group
         { groupName :: Maybe String
         , groupSubGlyphs :: [Glyph]
         }
 
-parseKVG :: String -> Maybe KVG
-parseKVG = undefined
+xmlnsAttr = uqAttr "xmlns" "http://www.w3.org/2000/svg"
+xlinkAttr = X.Attr
+              X.QName{ qName = "xlink", qURI = Nothing, qPrefix = "xmlns" }
+              "http://www.w3.org/1999/xlink"
 
-writeKVG :: KVG -> String
-writeKVG = undefined
+parseKvg :: String -> Maybe Glyph
+parseKvg kvg = do
+    xml <- XI.parseXMLDoc kvg
+    return $  xml
+
+writeKvg :: Glyph -> String
+writeKvg glyph = XO.showTopElement $ kvg
+  where
+    kvg = X.unode "svg" (attrs, writeKvgXml glyph)
+    attrs =
+      [ xmlnsAttr
+      , uqAttr "width"   "109"
+      , uqAttr "height"  "109"
+      , uqAttr "viewBox" "0 0 109 109"
+      ]
+
+parseKvgXml :: X.Element -> Maybe Glyph
+parseKvgXml xml
+
+writeKvgXml :: Glyph -> X.Element
+writeKvgXml = \case
+    Path{..}  -> X.unode "path" $ mkAttr "d" pathData
+    Group{..} -> X.unode "g" $ ([mkAttr "element" groupName], map writeXML groupSubGlyphs)
+
+renderSvg :: Glyph -> String
+renderSvg glyph = XO.showTopElement $ svg
+  where
+    svg = X.unode "svg" (attrs, renderSvgXml glyph)
+    attrs =
+      [ xmlnsAttr
+      , xlinkAttr
+      , uqAttr "width"   "109"
+      , uqAttr "width"   "109"
+      , uqAttr "viewBox" "0 0 109 109"
+      ]
+
+renderSvgXml :: Glyph -> X.Element
+renderSvgXml = undefined
+
+uqAttr :: String -> String -> X.Attr
+uqAttr = X.Attr . X.unqual
